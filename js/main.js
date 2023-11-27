cfd80053bd099aaa6e0f51d0f676015d58e931b6
Phoboz <122142766+phobitoz@users.noreply.github.com>
2023-11-27 03:02:05 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-// Obtener referencia al título h1
-const title = document.querySelector('h1');
+// Obtener referencias a los enlaces de la barra de navegación
+const navLinks = document.querySelectorAll('nav ul li a');
 
-// Agregar evento de clic al título para la animación
-title.addEventListener('click', () => {
-    title.style.animation = 'bounce 1s infinite';
+// Asignar un evento de clic a cada enlace
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    // Ocultar todas las secciones
+    document.querySelectorAll('main section').forEach(section => {
+      section.style.display = 'none';
+    });
+
+    // Mostrar la sección correspondiente al enlace clickeado
+    const targetId = link.getAttribute('href').substring(1);
+    document.getElementById(targetId).style.display = 'block';
+  });
 });
