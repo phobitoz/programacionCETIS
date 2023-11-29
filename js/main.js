dde757f2b85516e14cb5578bf567829d127303de
Phoboz <122142766+phobitoz@users.noreply.github.com>
2023-11-29 01:54:25 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,3 +14,16 @@ navLinks.forEach(link => {
     document.getElementById(targetId).style.display = 'block';
   });
 });
+
+/*Open/close the hamburger menu by toggling CSS classes as follows:*/
+
+const hamburger = document.querySelector(".hamburger")
+const nav_menu = document.querySelector(".nav-menu")
+hamburger.addEventListener("click",()=>{
+  hamburger.classList.toggle("active");
+  nav_menu.classList.toggle("active");
+})
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  nav_menu.classList.remove("active");
+}))
